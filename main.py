ed8240e185f98591cc0cadf67fceaeeba6a8c1ce
--unset-all <kjh041216-@naver.com>
2024-06-03 17:31:41 +0900

    game setting constants

---
+++ main.py
@@ -0,0 +1,12 @@
+import random, pygame, sys
+from pygame.locals import *
+
+FPS = 13
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+CELLSIZE = 20
+assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
+assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
+CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
+CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE)
+
